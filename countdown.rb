2da996a23d78e6eaba62f372f2e1484ba321ab73
Victoria Curl <victoriacurl9@gmail.com>
2019-12-20 16:06:45 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
-#write your code here
+def counterdown(int)
 
-def countdown
+  while int > 0 do
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep (5.seconds)
+  end
